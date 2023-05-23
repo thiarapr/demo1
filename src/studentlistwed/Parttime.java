dc49b3020de5fdfa358dedbfd625eaa1aefac6d0
prabh <thiarapr@sheridancollege.ca>
2023-05-23 15:27:41 -0400

    new Branch

---
+++ src/studentlistwed/Parttime.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlistwed;
+
+/**
+ *
+ * @author prabh
+ */
+public class Parttime {
+     public static void main(String[] args){
+         System.out.println("Hello World");
+    }
+   
+    
+}
